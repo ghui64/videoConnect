50930bb67fba3cc95f7a624d4791d712ec9dee8f
ghui64 <60405227+ghui64@users.noreply.github.com>
2020-05-07 23:36:09 -0400

    Updates

---
+++ precache-manifest.b1c71d1e3013c76e2b005674accaecae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09ad579746c107df30160238f7f39a1",
+    "url": "/VideoConnect/index.html"
+  },
+  {
+    "revision": "d4d3e331f43c9a5528f0",
+    "url": "/VideoConnect/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "41d85406d28d57defc67",
+    "url": "/VideoConnect/static/js/2.5338c671.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VideoConnect/static/js/2.5338c671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d3e331f43c9a5528f0",
+    "url": "/VideoConnect/static/js/main.87906102.chunk.js"
+  },
+  {
+    "revision": "b48e39e36851a944f613",
+    "url": "/VideoConnect/static/js/runtime-main.c3c380dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VideoConnect/static/media/logo.5d5d9eef.svg"
+  }
+]);
